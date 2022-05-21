234be0fa7958aa6e667ede09ad6c55174046b259
DaliRomero <104731849+DaliRomero@users.noreply.github.com>
2022-05-21 18:53:02 -0500

    Add files via upload

---
+++ nltk-urlib/Moreno Olmos Luis Miguel/Programa1189.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 18:19:51 2022
+
+@author: luis_
+"""
+
+import nltk
+from nltk import word_tokenize
+from urllib import request, response
+
+url = "https://1uismoreno.github.io/PLN/PLN.txt"
+
+response = request.urlopen(url)
+rawAlejandro = response.read().decode('utf8')
+
+tokens = word_tokenize(rawAlejandro)
+
+print("Tokens es: ",type(tokens),"\n")
+print("Cantidad de tokens: ",len(tokens),"\n")
+print("Primeros 15 tokens: ",tokens[:15],"\n")
+
+text = nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PLN")
