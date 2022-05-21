dd0fc092dda8b0e32099b93745d33f329c36fef4
cow01 <95662557+cow01@users.noreply.github.com>
2022-05-21 13:47:07 -0500

    Add files via upload

---
+++ nltk-urlib/AxelCruz/txtweb.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk import word_tokenize
+
+from urllib import request, response
+
+url="https://cow01.github.io/NLPWEB/Arfinal.txt"
+response = request.urlopen(url)
+raw=response.read().decode('utf8')
+
+tokens = word_tokenize(raw)
+print(f"\n{type(tokens)}")
+print(f"\nCantidad texto: {len(tokens)}")
+print(f"\n{tokens[:15]}")
+
+text=nltk.Text(tokens)
+print("\n")
+text.findall("<algoritmo><.*>")
+print("\n")
+text.concordance("algoritmo")
