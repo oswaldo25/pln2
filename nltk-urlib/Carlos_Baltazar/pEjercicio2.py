ec66c1c8ff4d15ee86c2399355eaace9a7997697
Carlos Alejandro Baltazar <104647514+Zarlbalt@users.noreply.github.com>
2022-05-22 17:58:00 -0500

    Add files via upload

---
+++ nltk-urlib/Carlos_Baltazar/pEjercicio2.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk import word_tokenize
+from urllib import request, response
+
+url = "https://github.com/Zarlbalt/PLN2/blob/main/act2/texto.txt"
+response = request.urlopen(url)
+raw = response.read().decode('utf8')
+
+tokens = word_tokenize(raw)
+print(f"\n{type(tokens)}")
+print(f"\n{len(tokens)}")
+print(f"\n{tokens[:15]}")
+
+text = nltk.Text(tokens)
+text.findall("<BTC>(<.*>)")
+text.concordance("BTC")
