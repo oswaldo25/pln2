2cd34dc182b40a154940e35b61d1f6654f84f44e
MarcoAL76 <104850809+MarcoAL76@users.noreply.github.com>
2022-05-21 19:21:22 -0500

    Add files via upload

---
+++ nltk-urlib/MarcoAlvarez/programa.py
@@ -0,0 +1,19 @@
+'''Marco Antonio Alvarez Lopez'''
+
+import nltk
+from nltk import word_tokenize
+
+from urllib import request
+
+url="https://marcoal76.github.io/plnprueba/Ac.txt"
+response = request.urlopen(url)
+raw=response.read().decode('utf8')
+
+tokens = word_tokenize(raw)
+print(f"\n{type(tokens)}")
+print(f"\n{len(tokens)}")
+print(f"\n{tokens[:15]}")
+
+text=nltk.Text(tokens)
+text.findall("<maquina><.*>")
+text.concordance("maquina")
