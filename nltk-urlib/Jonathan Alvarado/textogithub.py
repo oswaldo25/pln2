e5fa32b575cc5a1e3d66598c475100b13d3c74b4
JonyAv <104851140+JonyAv@users.noreply.github.com>
2022-05-22 22:26:31 -0500

    Add files via upload

---
+++ nltk-urlib/Jonathan Alvarado/textogithub.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk import word_tokenize
+from urllib import request
+
+url = "https://jonyav.github.io/fime/pagina.html"
+response = request.urlopen(url)
+raw = response.read().decode('utf8')
+
+tokens = word_tokenize(raw)
+print(type(tokens))
+print(len(tokens))
+print(tokens[:15])
+
+text = nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PLN")
