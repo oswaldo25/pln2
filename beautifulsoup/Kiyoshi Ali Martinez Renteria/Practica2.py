d32fe6d5b063477e926a9c5cee660d1a5f5272e7
Kmartinez24 <98865024+Kmartinez24@users.noreply.github.com>
2022-05-24 11:20:59 -0500

    Add files via upload

---
+++ beautifulsoup/Kiyoshi Ali Martinez Renteria/Practica2.py
@@ -0,0 +1,23 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import nltk
+from nltk import word_tokenize
+
+url = "https://www.iic.uam.es/inteligencia/que-es-procesamiento-del-lenguaje-natural/"
+
+html = urllib.request.urlopen(url)
+
+htmlParse = BeautifulSoup(html, 'html.parser')
+
+tokens = []
+for para in htmlParse.find_all("p"):
+    tokens = tokens + word_tokenize(para.get_text())
+
+
+print(type(tokens))
+print(len(tokens))
+print(tokens)
+
+text = nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PLN")
