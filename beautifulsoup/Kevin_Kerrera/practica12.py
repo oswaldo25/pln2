4da0cefe3ae915077e69632f1bc57ccb7b2f3535
KevinHerrera21 <104850715+KevinHerrera21@users.noreply.github.com>
2022-05-24 11:37:08 -0500

    Create practica12.py

---
+++ beautifulsoup/Kevin_Kerrera/practica12.py
@@ -0,0 +1,18 @@
+import nltk
+from urllib import request
+from bs4 import BeautifulSoup
+
+url = "https://kevinherrera21.github.io/ComputoNube/views/estado.html"
+response = request.urlopen(url)
+contenidoPagina = BeautifulSoup(response,'html.parser')
+
+text = contenidoPagina.get_text()
+tokens = nltk.word_tokenize(text)
+print(text)
+print(f"type: {type(tokens)}")
+print(f"Total tokens: {len(tokens)}")
+print(tokens[:15])
+
+text_tokens = nltk.Text(tokens)
+text_tokens.findall("<nube>(<.*>)")
+text_tokens.concordance("nube")
