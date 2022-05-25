1b6d68876ccdd0f8365bd697b3bad9338d6c7fa2
Jazmin Gonzalez Peredia <104851181+jazminGOPE@users.noreply.github.com>
2022-05-24 19:44:03 -0500

    Update programa7_2P.py

--- hermosa s/Jazmín González/programa7_2P.py
+++ hermosa s/Jazmín González/programa7_2P.py
@@ -1,1 +1,23 @@
+#Actividad 7/ Parcial 2
+#Jazmin González
 
+import urllib.request
+from bs4 import BeautifulSoup
+import nltk
+from nltk import word_tokenize
+
+url = "https://www.viewnext.com/que-es-pln-procesamiento-lenguaje-natural/"
+html = urllib.request.urlopen(url)
+htmlParse = BeautifulSoup(html, 'html.parser')
+
+tokens = []
+for para in htmlParse.find_all("p"):
+    tokens = tokens + word_tokenize(para.get_text())
+
+print(type(tokens))
+print(len(tokens))
+print(tokens)
+
+text = nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PLN")
