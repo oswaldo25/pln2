34750057b7cfa628d1f5be9e7941e87636fd0717
FerCDD <104850704+FerCDD@users.noreply.github.com>
2022-05-24 20:49:42 -0500

    Add files via upload

---
+++ beautifulsoup/FernandaCastillo/bs.py
@@ -0,0 +1,25 @@
+'''Fernanda Castillo'''
+
+import nltk
+from nltk import word_tokenize
+from bs4 import BeautifulSoup
+from urllib import request
+
+
+url="https://decidesoluciones.es/procesamiento-del-lenguaje-natural-pln-o-nlp-que-es-y-para-que-se-utiliza/"
+response = request.urlopen(url)
+
+readhtml=BeautifulSoup(response,'html.parser')
+
+tokens = []
+
+for parsers in readhtml.find_all("p"):
+    tokens=tokens + word_tokenize(parsers.get_text())
+
+print(type(tokens))
+print("Total de palabras: ",len(tokens))
+print(tokens)
+
+text=nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PLN")
