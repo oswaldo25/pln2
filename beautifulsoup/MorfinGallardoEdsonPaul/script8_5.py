a2eae6a11fceac3f4577253eb0a804fcc32d4e96
Edson Paul <73849076+EdsonMorfin@users.noreply.github.com>
2022-05-26 19:12:02 -0500

    Add files via upload

---
+++ beautifulsoup/MorfinGallardoEdsonPaul/script8_5.py
@@ -0,0 +1,29 @@
+8 lines (23 sloc) 711 Bytes
+from matplotlib.pyplot import text
+import nltk
+from nltk import word_tokenize
+# importing modules
+# import urllib.request
+from urllib import request
+from bs4 import BeautifulSoup
+
+# providing url
+url = "https://decidesoluciones.es/procesamiento-del-lenguaje-natural-pln-o-nlp-que-es-y-para-que-se-utiliza/"
+
+# opening the url for reading
+response = request.urlopen(url)
+
+# parsing the html file
+contenidoPagina = BeautifulSoup(response, 'html.parser')
+text = contenidoPagina.get_text()
+tokens = word_tokenize(text)
+title = contenidoPagina.title
+
+print(title)
+print(type(tokens))
+print(len(tokens))
+print(tokens)
+
+text_tokens = nltk.Text(tokens)
+text_tokens.findall("<PLN>(<.*>)")
+text_tokens.concordance("PLN")
