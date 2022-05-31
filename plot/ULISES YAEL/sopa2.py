7dd0e23087809babcb8d07f2c57100fe1bfbc047
UlisesY <104850572+UlisesY@users.noreply.github.com>
2022-05-31 11:50:24 -0500

    Grafica de la pagina web

---
+++ plot/ULISES YAEL/sopa2.py
@@ -0,0 +1,50 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import nltk
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+
+url = "https://www.tableau.com/es-mx/learn/articles/natural-language-processing-examples"
+
+html = urllib.request.urlopen(url)
+
+htmlParse = BeautifulSoup(html, 'html.parser')
+
+tokens = []
+for para in htmlParse.find_all("p"):
+    tokens = tokens + word_tokenize(para.get_text())
+
+texto = htmlParse.getText()
+
+title = htmlParse.title
+print("----------------------------------")
+print("Titulo: ")
+print(title)
+print("----------------------------------")
+print(type(tokens))
+print(len(tokens))
+print(tokens)
+
+text = nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PLN")
+htmlwords = ['https', 'http', 'display', 'button', 'hover',
+             'color', 'background', 'height', 'none', 'target',
+             'WebPage', 'reload', 'fieldset', 'padding', 'input',
+            'select', 'textarea', 'html', 'form', 'cursor',
+            'overflow', 'format', 'italic', 'normal', 'truetype',
+            'before', 'name', 'label', 'float', 'title', 'arial', 'type',
+            'block', 'audio', 'inline', 'canvas', 'margin', 'serif', 'menu',
+            'woff', 'content', 'fixed', 'media', 'position', 'relative', 'hidden',
+            'width', 'clear', 'body', 'standard', 'expandable', 'helvetica',
+            'fullwidth', 'embed', 'expandfull', 'fullstandardwidth', 'left', 'middle',
+            'iframe', 'rgba', 'selected', 'scroll', 'opacity',
+            'center', 'false', 'right', 'cookies']
+
+words = [w for w in tokens if w.isalpha() and len(w) > 4 and w.lower() not in htmlwords]
+
+text2 = nltk.Text(words)
+
+freqdist = FreqDist(text2)
+freqdist.plot(20)
+freqdist.plot(20, cumulative=True)
