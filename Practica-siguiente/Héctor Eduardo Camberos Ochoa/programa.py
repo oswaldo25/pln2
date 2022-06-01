f1a58f6a611e73f2f886661a002d15f174e75146
HCamberos <56201325+HCamberos@users.noreply.github.com>
2022-06-01 13:26:11 -0500

    Add files via upload

---
+++ Practica-siguiente/Héctor Eduardo Camberos Ochoa/programa.py
@@ -0,0 +1,31 @@
+'''
+Programa realizado por:
+    Héctor Eduardo Camberos Ochoa
+    ICI 6°B
+'''
+
+import urllib.request
+from bs4 import BeautifulSoup
+import nltk
+from nltk import word_tokenize
+url = "https://www.iic.uam.es/inteligencia/que-es-procesamiento-del-lenguaje-natural/"
+
+html = urllib.request.urlopen(url)
+
+htmlParse = BeautifulSoup(html,'html.parser')
+
+tokens =[]
+for para in htmlParse.find_all("p"):
+    tokens = tokens + word_tokenize(para.get_text())
+
+title = htmlParse.title
+print("------------------------------------------")
+print(title)
+print("------------------------------------------")
+print(type(tokens))
+print(len(tokens))
+print(tokens)
+
+text= nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+text.concordance("PNL")
