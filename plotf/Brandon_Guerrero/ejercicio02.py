7357523bf6700535bc7ea66ed0e30f58a0f9f4b4
BRANDON YAIR GUERRERO GÓMEZ <74810024+b-guerrero@users.noreply.github.com>
2022-05-31 19:12:33 -0500

    Add files via upload

---
+++ plotf/Brandon_Guerrero/ejercicio02.py
@@ -0,0 +1,49 @@
+import nltk
+import urllib.request
+from bs4 import BeautifulSoup
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+
+
+url = "https://b-guerrero.github.io/Natural-Language-Processing/"
+html = urllib.request.urlopen(url)
+htmlParse = BeautifulSoup(html, 'html.parser')
+
+tokens = []
+for para in htmlParse.find_all("p"):
+  tokens = tokens + word_tokenize(para.get_text())
+
+title = htmlParse.title
+print(f"\nTitulo: {title}")
+print(f"\n{type(tokens)}")
+print(f"\n{len(tokens)}")
+print(f"\n{tokens}")
+
+text = nltk.Text(tokens)
+text.findall("<PLN>(<.*>)")
+print("\n")
+text.concordance("PLN")
+
+htmlwords = ['https', 'http', 'display', 'button', 'hover',
+            'color', 'background', 'height', 'none', 'target',
+            'WebPage', 'reload', 'fieldset', 'padding', 'input',
+            'select', 'textarea', 'html', 'form', 'cursor',
+            'overflow', 'format', 'italic', 'normal', 'truetype',
+            'before', 'name', 'label', 'float', 'title', 'arial', 'type',
+            'block', 'audio', 'inline', 'canvas', 'margin', 'serif', 'menu',
+            'woff', 'content', 'fixed', 'media', 'position', 'relative', 'hidden',
+            'width', 'clear', 'body', 'standard', 'expandable', 'helvetica',
+            'fullwidth', 'embed', 'expandfull', 'fullstandardwidth', 'left', 'middle',
+            'iframe', 'rgba', 'selected', 'scroll', 'opacity',
+            'center', 'false', 'right', 'cookies']
+
+#\par
+words = [w for w in tokens if w.isalpha() and len(w) > 4 and w.lower() not in htmlwords]
+
+# Create NLTK Text instance to use NLTK APIs\par
+text2 = nltk.Text(words)
+
+# Create Frequency distribution to see frequency of words\par
+freqdist = FreqDist(text2)
+freqdist.plot(20)
+freqdist.plot(20, cumulative=True)
